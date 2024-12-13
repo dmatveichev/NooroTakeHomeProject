e6563da219f20959d6f60096d5b30c9efda9c45b
Dmitrii Matveichev <dmatveichev@icloud.com>
2024-12-13 11:50:07 -0500

    Initial Commit

---
+++ NooroTakeHome/NooroTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NooroTakeHomeApp.swift
+//  NooroTakeHome
+//
+//  Created by Dmitry Matveichev on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct NooroTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
